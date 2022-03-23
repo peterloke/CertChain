c6a38c3ae6cde359eafc6ce4032f10aced4a2b1d
peterloke <70332682+peterloke@users.noreply.github.com>
2022-03-23 16:37:18 +0800

    Add files via upload

---
+++ test/test_certchain.js
@@ -0,0 +1,14 @@
+const _deploy_contracts = require("../migrations/2_deploy_contracts");
+const truffleAssert = require('truffle-assertions');
+var assert = require('assert');
+
+var Pool = artifacts.require("../contracts/CertChain.sol");
+
+contract('CertChain', function(accounts) {
+    before(async () => {
+        poolInstance = await Pool.deployed();
+    });
+
+    it('Test 1', async() => {
+    });
+});
